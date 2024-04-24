2878a74e2de6c88b62f671192a37820b22e716ad
guusfelix2015 <gusfelix2015@gmail.com>
2024-04-23 23:13:03 -0300

    feat: get user autenticated

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -6,7 +6,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  me() {
-    return this.usersService.getUserById('user id');
+  me(@Req() request: any) {
+    return this.usersService.getUserById(request.userId);
   }
 }
