a994428d3eb9376bbea49b8a5dcdc7b620198d81
guusfelix2015 <gusfelix2015@gmail.com>
2024-04-23 23:13:16 -0300

    feat: get user by id

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly userRepo: UserRepository) {}
+
   getUserById(userId: string) {
-    return { userId };
+    return this.userRepo.findUnique({
+      where: { id: userId },
+      select: {
+        name: true,
+        email: true,
+      },
+    });
   }
 }
