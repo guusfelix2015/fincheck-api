50844c95c316aaf32e455f9898332d2bd09c41e1
guusfelix2015 <gusfelix2015@gmail.com>
2024-04-24 21:13:47 -0300

    feat: add repositories in database module

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -2,10 +2,16 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UserRepository } from './repositories/users.repositories';
 import { CategoriesRepository } from './repositories/categories.repositories';
+import { BankAccountsRepository } from './repositories/bank-accounts.repositories';
 
 @Global()
 @Module({
-  exports: [UserRepository, CategoriesRepository],
-  providers: [PrismaService, UserRepository, CategoriesRepository],
+  exports: [UserRepository, CategoriesRepository, BankAccountsRepository],
+  providers: [
+    PrismaService,
+    UserRepository,
+    CategoriesRepository,
+    BankAccountsRepository,
+  ],
 })
 export class DatabaseModule {}
